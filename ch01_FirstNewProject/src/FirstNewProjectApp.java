4f835f5c71442b15b6b8746e1186eafe8fedd10a
Emilie Nerl <emilie.nerl1@gmail.com>
2020-08-31 18:31:26 -0400

    first commit

---
+++ ch01_FirstNewProject/src/FirstNewProjectApp.java
@@ -0,0 +1,10 @@
+
+public class FirstNewProjectApp {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("there!");
+
+	}
+
+}
---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,11 @@
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		 System.out.println("Success!");
+		 System.out.println("Success again!");
+		 System.out.println("Triple success!");
+
+	}
+
+}
