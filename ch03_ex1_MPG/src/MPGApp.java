a3dcec19ea5855d7afc6fb518341d44172225708
Emilie Nerl <emilie.nerl1@gmail.com>
2020-09-09 20:11:41 -0400

    additional code work from class for this exercise

--- ch03_ex1_MPG/src/MPGApp.java
+++ ch03_ex1_MPG/src/MPGApp.java
@@ -1,4 +1,5 @@
-import java.math.BigDecimal;
+
+/*import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Scanner;
@@ -41,6 +42,57 @@ public class MPGApp {
 
 			BigDecimal mpg = miles.divide(gallons, 2, RoundingMode.HALF_UP);
 			System.out.println("Miles per gallon is " + mpg.toString() + ".");
+
+			System.out.println();
+
+			System.out.print("Calculate another MPG? (y/n): ");
+			choice = sc.next();
+			System.out.println();
+		}
+	}
+
+}*/
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class MPGApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Miles Per Gallon calculator");
+		System.out.println(); // print a blank line
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter miles driven: ");
+			double miles = sc.nextDouble();
+
+			System.out.print("Enter gallons of gas used: ");
+			double gallons = sc.nextDouble();
+
+			// code for steps 3,4,5
+			// double mpg = miles / gallons;
+			// mpg = (double) Math.round(mpg * 100) / 100;
+			// long mpgRounded = Math.round(mpg);
+
+			/*
+			 * double mpg = miles / gallons; NumberFormat mpgFormatter =
+			 * NumberFormat.getNumberInstance(); mpgFormatter.setMaximumFractionDigits(3);
+			 * 
+			 * String mpgFormatted = mpgFormatter.format(mpg);
+			 * 
+			 * System.out.println("Miles per gallon is " + mpgFormatted + ".");
+			 */
+
+			BigDecimal milesBigDec = new BigDecimal(miles);
+			BigDecimal gallonsBigDec = new BigDecimal(gallons);
+			BigDecimal mpgBigDec = milesBigDec.divide(gallonsBigDec, 2, RoundingMode.HALF_UP);
+
+			System.out.println("Miles per gallon is " + mpgBigDec + ".");
 
 			System.out.println();
 
