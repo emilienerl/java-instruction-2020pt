fe6f93596783f322da8dcc9a2c06758d5d6ed02d
Emilie Nerl <emilie.nerl1@gmail.com>
2020-09-26 09:39:19 -0400

    chapter 8 and 9 instruction

--- ch08_Project8_2PersonManager/src/PersonManagerApp.java
+++ ch08_Project8_2PersonManager/src/PersonManagerApp.java
@@ -2,7 +2,50 @@
 public class PersonManagerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to the Person Manager");
+		System.out.println();
+
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+
+			String custOrEmp = Console.getString("Create customer or employee? (c/e) ");
+
+			boolean isValid = false;
+			if (custOrEmp.equalsIgnoreCase("C") || custOrEmp.equalsIgnoreCase("E")) {
+				isValid = true;
+			}
+			if (!isValid) {
+				System.out.println("Error! this entry is required. Try again.");
+				continue;
+			}
+
+			System.out.println();
+			String firstName = Console.getString("First Name: ");
+			String lastName = Console.getString("Last Name: ");
+
+			if (custOrEmp.equalsIgnoreCase("C")) {
+				String custNum = Console.getString("Customer number: ");
+
+				Customer customer = new Customer(firstName, lastName, custNum);
+
+				System.out.println();
+				System.out.println("You entered a new Customer");
+				System.out.println(customer);
+
+			} else {
+				String ssn = Console.getString("SSN: ");
+
+				Employee employee = new Employee(firstName, lastName, ssn);
+
+				System.out.println();
+				System.out.println("You entered a new Employee");
+				System.out.println(employee);
+			}
+			System.out.println();
+			choice = Console.getString("Continue? (y/n) ");
+			System.out.println();
+		}
 
 	}
 
